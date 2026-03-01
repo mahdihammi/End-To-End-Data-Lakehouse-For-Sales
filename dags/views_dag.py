4e6df7e9fe6b93bb67c4e426cd65f84949fb6e89
mahdihammi <mahdihammi12@gmail.com>
2026-03-01 13:17:15 +0100

    feat(bronze/silver) : Added the testing dag

--- dags/views_dag.py
+++ dags/views_dag.py
@@ -6,6 +6,40 @@ from airflow.providers.standard.operators.python import PythonOperator
 from datetime import datetime
 from include.medall_arch.bronze_layer import BronzeLayerManager
 from include.medall_arch.silver_layer import SilverLayerManager
+from include.medall_arch.views import creating_views
+
+
+
+
+
+TABLE_NAME = "orders"
+BUCKET_NAME = "lakehouse-project"
+BRONZE_PREFIX = "lakehouse-raw/sales"
+WATERMARK_VAR = "sales_last_updated_at"
+
+DBNAME = "postgres"
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "minio:9000")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "minioadmin")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "minioadmin")
+
+SUPABASE_HOST = os.getenv("SUPABASE_HOST")
+SUPABASE_PORT = os.getenv("SUPABASE_PORT")
+SUPABASE_USER = os.getenv("SUPABASE_USER")
+SUPABASE_PWD = os.getenv("SUPABASE_PWD")
+DUCKDB_SECRET = os.getenv('DUCKDB_SECRET')
+
+
+
+@dag(
+    dag_id="view_dag_test",
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+)
+
+
+
+
 
 
 
