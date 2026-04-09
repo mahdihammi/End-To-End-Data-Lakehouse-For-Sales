b2b35de76f6a6a0aa196f3a881ceb0f3fde583d7
mahdihammi <mahdihammi12@gmail.com>
2026-04-10 00:39:01 +0100

    feat(silver-layer): test refactoring code to use DDL and keep MERGE do the bulk insert and upsert

--- dags/dag_test_new_silver.py
+++ dags/dag_test_new_silver.py
@@ -5,7 +5,7 @@ from airflow.providers.standard.operators.python import PythonOperator
 
 from datetime import datetime
 from include.medall_arch.bronze_layer import BronzeLayerManager
-from include.medall_arch.silver_layer_test import SilverLayerManager
+from include.medall_arch_test.silver_layer_test import SilverLayerManager
 from include.medall_arch.gold_layer import GoldTableManager
 from dotenv import load_dotenv
 import os
